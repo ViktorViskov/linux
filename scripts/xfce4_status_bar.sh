183c4b696aff4dae237799a4dcce6e830d43ba10
Viktor Viskov <carrergt@gmail.com>
2021-08-17 11:53:07 +0200

    Fix merge conflict

--- scripts/xfce4_status_bar.sh
+++ scripts/xfce4_status_bar.sh
@@ -8,6 +8,7 @@ CPU_Freq_Current="`lscpu | grep 'CPU МГц' | awk '{printf "%.1f", $3/1000}'`GH
 CPU_Freq_Max="`cat /etc/tlp.conf | grep CPU_SCALING_MAX_FREQ_ON_BAT | awk -F'=' '{printf "%.1f", $2 / 1000000}'`GHz"
 CPU_Turbo=`cat /etc/tlp.conf | grep CPU_BOOST_ON_BAT | awk -F'=' '{print $2}'`
 Battery_Safe=`cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode`
+Up_Time=`uptime -p | uptime -p | awk '{printf "%d:", $2; printf "%02d", $4}'`
 
 # cpu turbo boost logick
 if [ $CPU_Turbo = '0' ]; then
@@ -24,6 +25,5 @@ else
     Battery_Safe="<span weight=\"bold\" fgcolor=\"#E9967A\">Battery Save ON</span>"
 fi
 
-cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode
 # print result
-echo "<txt><span weight=\"bold\" fgcolor=\"Green\">CPU $CPU_Temp</span><span weight=\"bold\" fgcolor=\"Orange\"> $CPU_Freq_Current</span><span weight=\"bold\" fgcolor=\"Red\"> $CPU_Freq_Max </span>$CPU_Turbo<span weight=\"bold\" fgcolor=\"Cyan\"> RAM $RAM </span>$Battery_Safe</txt>"
+echo "<txt><span weight=\"bold\" fgcolor=\"Green\">CPU $CPU_Temp</span><span weight=\"bold\" fgcolor=\"Orange\"> $CPU_Freq_Current</span><span weight=\"bold\" fgcolor=\"Red\"> $CPU_Freq_Max </span>$CPU_Turbo<span weight=\"bold\" fgcolor=\"Cyan\"> RAM $RAM </span>$Battery_Safe<span> UP $Up_Time</span></txt>"
