73bf876856a8f0afbdafb4e346622cd85d0c9055
Viktor Viskov <carrergt@gmail.com>
2023-03-06 21:10:52 +0100

    modified:   scripts/http_server.sh 	modified:   scripts/media/load_wallpapers.sh

--- scripts/http_server.sh
+++ scripts/http_server.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/sh
-python3 -m http.server 8080
+if [ $1 ]
+then
+	sudo python3 -m http.server $1
+else
+	sudo python3 -m http.server 80
+fi
--- scripts/media/load_wallpapers.sh
+++ scripts/media/load_wallpapers.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/bash
 wget https://source.unsplash.com/1920x1080?landscape -O $HOME/.config/bg.jpg
