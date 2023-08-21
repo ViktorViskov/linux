26b393e7f2c2af45841774ef081c9a7d9be1ab3f
Viktor Viskov <carrergt@gmail.com>
2023-08-21 15:59:43 +0200

    Added script to install de

---
+++ scripts/custom_de.sh
@@ -0,0 +1,36 @@
+sudo apt update
+sudo apt install xorg \
+xinit \
+dunst \
+compton \
+curl \
+p7zip-full \
+htop \
+feh \
+axel \
+tmux \
+suckless-tools \
+ffmpeg \
+xbindkeys \
+nfs-common \
+python3-pip \
+python3-venv \
+vlc \
+gdebi \
+geany \
+xarchiver \
+network-manager-gnome \
+gpicview \
+lxrandr \
+lxpanel \
+lxappearance \
+lxtask \
+lxpolkit \
+obsession \
+xfwm4 \
+xfce4-clipman \
+xfce4-terminal \
+xfce4-screenshooter \
+thunar \
+-y
+
