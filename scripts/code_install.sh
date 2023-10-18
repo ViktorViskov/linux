1d1123ea627227d86c06cc42188b7e4a251b3e8b
Viktor Viskov <carrergt@gmail.com>
2023-10-18 20:39:46 +0200

    Add script to install dbeaver and change some settings

--- scripts/code_install.sh
+++ scripts/code_install.sh
@@ -1,2 +1,3 @@
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O code.deb
 sudo apt install ./code.deb
+rm code.deb
---
+++ scripts/dbeaver_install.sh
@@ -0,0 +1,3 @@
+wget "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" -O dbeaver.deb
+sudo apt install ./dbeaver.deb
+rm dbeaver.deb
